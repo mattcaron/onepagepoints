afbc82f78a8b5fa2c05fe3a65cdafb4a9d73e5c4
kdj0c <kdj0c@djinvi.net>
2017-10-06 00:45:52 +0200

    Automatic indent of unitsX.json and upgradesX.json

--- indentjson.py
+++ indentjson.py
@@ -22,27 +22,28 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
 """
-This scripts helps to generate the json files for a faction.
-It takes a libreoffice calc ".ods" file, and generate the list
-of weapons, and units.
-The libreoffice calc file can be done with just copy/paste from the original pdf,
-and a few "manual" operation
+This scripts helps to indent the json files for each faction
+equipments.json (list of weapons and wargear)
+unitsX.json (list of units for page X)
+upgradesX.json (list of upgrades available for each unit in page X)
 """
 
 import os
 import json
+import argparse
+
 
 def format_weapon(w):
     s = '{{ "range" : {:<2}, "attacks" : {:<2}, "ap" : {:<2}, "special" : {}}}'.format(w['range'], w['attacks'], w['ap'], json.dumps(w['special']))
     return s
 
-def main():
-    jsonFile = "Tao/equipments.json"
 
+def format_equipments(jsonFile):
+    print('processing {0}'.format(os.path.abspath(jsonFile)))
     with open(jsonFile, "r") as f:
         data = json.loads(f.read())
 
-    os.rename(jsonFile,jsonFile + "~")
+    os.rename(jsonFile, jsonFile + "~")
 
     weapons = data['weapons']
     wargear = data['wargear']
@@ -53,6 +54,88 @@ def main():
         f.write('\n}, "wargear" : {\n')
         f.write(",\n".join(['{:<30} : '.format('"' + w + '"') + json.dumps(wargear[w]) for w in sorted(wargear)]))
         f.write('\n}}\n')
+
+
+def format_unit(unit):
+    s = '  "name" : "{}",\n'.format(unit['name'])
+    s += '  "count" : {},\n'.format(unit['count'])
+    s += '  "quality" : {},\n'.format(unit['quality'])
+    s += '  "defense" : {},\n'.format(unit['defense'])
+    s += '  "equipment" : {},\n'.format(json.dumps(unit['equipment']))
+    s += '  "special" : {},\n'.format(json.dumps(unit['special']))
+    s += '  "upgrades" : {}\n'.format(json.dumps(unit['upgrades']))
+    return s
+
+
+def format_units(jsonFile):
+    print('processing {0}'.format(os.path.abspath(jsonFile)))
+    with open(jsonFile, "r") as f:
+        data = json.loads(f.read())
+
+    os.rename(jsonFile, jsonFile + "~")
+
+    with open(jsonFile, "w") as f:
+        f.write('[{\n')
+        f.write('},{\n'.join([format_unit(unit) for unit in data]))
+        f.write('}]\n')
+
+
+def format_batch(batch):
+    s = '      "text" : "{}",\n'.format(batch['text'])
+    if 'all' in batch and batch['all']:
+        s += '      "all" : true,\n'
+    if 'remove' in batch:
+        s += '      "remove" : {},\n'.format(json.dumps(batch['remove']))
+    s += '      "add" : {}\n'.format(json.dumps(batch['add']))
+    return s
+
+
+def format_group(groupName, group):
+    s = '"{0}" : [{{ \n'.format(groupName)
+    s += '    },{\n'.join([format_batch(batch) for batch in group])
+    return s
+
+
+def format_upgrades(jsonFile):
+    print('processing {0}'.format(os.path.abspath(jsonFile)))
+    with open(jsonFile, "r") as f:
+        data = json.loads(f.read())
+
+    os.rename(jsonFile, jsonFile + "~")
+
+    with open(jsonFile, "w") as f:
+        f.write('{\n')
+        f.write('}],\n'.join([format_group(group, data[group]) for group in sorted(data)]))
+        f.write('}]}\n')
+
+
+def format_faction():
+    allFiles = os.listdir(".")
+
+    equFile = 'equipments.json'
+    if equFile in allFiles:
+        format_equipments(equFile)
+
+    for f in allFiles:
+        if f.startswith('units') and f.endswith('.json'):
+            format_units(f)
+        if f.startswith('upgrades') and f.endswith('.json'):
+            format_upgrades(f)
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Indent the json source files, to ease editing them by hand, and avoid to much useless diffs')
+    parser.add_argument('path', metavar='path', type=str, nargs='+',
+                        help='path to the faction to indent all json')
+
+    args = parser.parse_args()
+
+    current_dir = os.getcwd()
+    for path in args.path:
+        os.chdir(path)
+        format_faction()
+        os.chdir(current_dir)
 
 
 if __name__ == "__main__":
